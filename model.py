fc370dde63abc2b263cf84102e9890edc3b0b21f
Kacper <68641248+neomat-prog@users.noreply.github.com>
2024-11-02 22:06:53 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,29 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class IrisNN(nn.Module):
-    def __init__(self):
+    def __init__(
+        self, input_size=4, hidden_sizes=[8, 16], output_size=3, dropout_rate=0.3
+    ):
         super(IrisNN, self).__init__()
-        self.fc1 = nn.Linear(4, 8)
-        self.fc2 = nn.Linear(8, 3)
+
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_sizes)):
+            if i == 0:
+                self.hidden_layers.append(nn.Linear(input_size, hidden_sizes[i]))
+            else:
+                self.hidden_layers.append(
+                    nn.Linear(hidden_sizes[i - 1], hidden_sizes[i])
+                )
+
+        self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        for layer in self.hidden_layers:
+            x = F.relu(layer(x))
+            x = self.dropout(x)
+        x = self.output_layer(x)
         return x
